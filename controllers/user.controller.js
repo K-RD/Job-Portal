d519f447e5395948725b5ba9e0804254133946e4
ROBIND KUMAR <kumarrobind386@gmail.com>
2024-09-29 09:20:38 +0530

    modified user login in user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -40,7 +40,7 @@ export const register = async (req, res) => {
   }
 };
 
-const login = async (req, res) => {
+export const login = async (req, res) => {
   try {
     const { email, password, role } = req.body;
     if (!email || !password || !role) {
