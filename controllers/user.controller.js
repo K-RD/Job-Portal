3aaa24dba22f8a80e0c6c5537cd0aae7e8b7ae6e
ROBIND KUMAR <kumarrobind386@gmail.com>
2024-09-29 09:16:43 +0530

    user controller for user registration

---
+++ controllers/user.controller.js
@@ -0,0 +1,40 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const register = async (req, res) => {
+  try {
+    const { fullname, email, phoneNumber, password, role } = req.body;
+    if (!fullname || !email || !phoneNumber || !password || !role) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all the fields",
+      });
+    }
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists with this email",
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+      fullname,
+      email,
+      phoneNumber,
+      password: hashedPassword,
+      role,
+    });
+
+    return res.status(201).json({
+      message: "Account created successfully",
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
