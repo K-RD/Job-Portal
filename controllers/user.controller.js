1acb60f108bdda16eb31a1a519525b331f25fdde
ROBIND KUMAR <kumarrobind386@gmail.com>
2024-09-29 09:26:05 +0530

    Add logout functionality to user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -113,3 +113,18 @@ export const login = async (req, res) => {
     });
   }
 };
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.status(200).json({
+      message: "Logged out successfully",
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
